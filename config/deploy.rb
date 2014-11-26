afcecd4582826279a05ca614383ce077c84342fe
Danilo Lima <danilo.lima@victus.com.br>
2014-11-26 15:50:28 -0200

    including secrets to cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, 'deploy@olimpo:/home/olimpo/projects/variables_app'
 
 set :deploy_to, '/home/deploy/variables_app'
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
