0fbc9e0819fd2b28f901b0f1ce9b1dbe7ddbc4dd
Danilo Lima <danilo.lima@victus.com.br>
2014-11-27 11:29:06 -0200

    make deploy run migrations

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,4 @@
 set :stage, :production
 set :branch, 'master'
-set :keep_releases, 5
 
-# Replace 127.0.0.1 with your server's IP address!
-server '192.168.1.96', user: 'deploy', roles: %w{web app}
+server '192.168.1.96', user: 'deploy', roles: %w{web app db}
