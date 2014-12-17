130515fe515c7bb8b3dcedbf7ce01631c4794867
Danilo Lima <danilo.lima@victus.com.br>
2014-12-17 09:32:13 -0200

    Changing status to constants

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,24 +2,24 @@ class WelcomeController < ApplicationController
   def index
     @filter = params[:status] || 'origin'
     if @filter == "origin"
-      @entity_status  = Origin.select( :id ).select(:status).select( :file_name ).where( :status => 'sala1').order( :id => :desc ).to_a,
-        Origin.select( :id ).select( :status).select( :file_name ).where( :status => 'sala2').order( :id => :desc ).to_a,
+      @entity_status  = Origin.select( :id ).select(:status).select( :file_name ).where( :status => Constants::STATUS[:SALA1]).order( :id => :desc ).to_a,
+        Origin.select( :id ).select( :status).select( :file_name ).where( :status => Constants::STATUS[:SALA2]).order( :id => :desc ).to_a,
         Origin.select( :id ).select(:status).select( :file_name ).where( :status => 'producao').order( :id => :desc ).to_a
     elsif  @filter == "campaign"
-      @entity_status  = Campaign.select( :id ).select(:status).select( :name ).where( :status => 'sala1').order( :id => :desc ).to_a,
-        Campaign.select( :id ).select( :status).select( :name ).where( :status => 'sala2').order( :id => :desc ).to_a,
+      @entity_status  = Campaign.select( :id ).select(:status).select( :name ).where( :status => Constants::STATUS[:SALA1]).order( :id => :desc ).to_a,
+        Campaign.select( :id ).select( :status).select( :name ).where( :status => Constants::STATUS[:SALA2]).order( :id => :desc ).to_a,
         Campaign.select( :id ).select(:status).select( :name ).where( :status => 'producao').order( :id => :desc ).to_a
     elsif  @filter == "variable"
-      @entity_status  = Variable.select( :id ).select(:status).select( :name ).where( :status => 'sala1').order( :id => :desc ).to_a,
-        Variable.select( :id ).select( :status).select( :name ).where( :status => 'sala2').order( :id => :desc ).to_a,
+      @entity_status  = Variable.select( :id ).select(:status).select( :name ).where( :status => Constants::STATUS[:SALA1]).order( :id => :desc ).to_a,
+        Variable.select( :id ).select( :status).select( :name ).where( :status => Constants::STATUS[:SALA2]).order( :id => :desc ).to_a,
         Variable.select( :id ).select(:status).select( :name ).where( :status => 'producao').order( :id => :desc ).to_a
     elsif  @filter == "table"
-      @entity_status  = Table.select( :id ).select(:status).select( :name ).where( :status => 'sala1').order( :id => :desc ).to_a,
-        Table.select( :id ).select( :status).select( :name ).where( :status => 'sala2').order( :id => :desc ).to_a,
+      @entity_status  = Table.select( :id ).select(:status).select( :name ).where( :status => Constants::STATUS[:SALA1]).order( :id => :desc ).to_a,
+        Table.select( :id ).select( :status).select( :name ).where( :status => Constants::STATUS[:SALA2]).order( :id => :desc ).to_a,
         Table.select( :id ).select(:status).select( :name ).where( :status => 'producao').order( :id => :desc ).to_a
     elsif  @filter == "processid"
-      @entity_status  = Processid.select( :id ).select(:status).select( :mnemonic ).where( :status => 'sala1').order( :id => :desc ).to_a,
-        Processid.select( :id ).select( :status).select( :mnemonic ).where( :status => 'sala2').order( :id => :desc ).to_a,
+      @entity_status  = Processid.select( :id ).select(:status).select( :mnemonic ).where( :status => Constants::STATUS[:SALA1]).order( :id => :desc ).to_a,
+        Processid.select( :id ).select( :status).select( :mnemonic ).where( :status => Constants::STATUS[:SALA2]).order( :id => :desc ).to_a,
         Processid.select( :id ).select(:status).select( :mnemonic ).where( :status => 'producao').order( :id => :desc ).to_a
     end
 
