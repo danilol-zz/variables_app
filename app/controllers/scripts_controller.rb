b42dfa5830595b1ac6b72861f07427c6d8a7ce82
= <alison.souza@itau-unibanco.com.br>
2014-12-22 16:23:35 -0200

    new screen to generete output scrpits

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -3,6 +3,15 @@ class ScriptsController < ApplicationController
   before_filter :ensure_authentication, :only => [:index]
 
   def index
+
+  end
+
+  def generate_script
+  	sprint = params["sprint_number"].to_i
+  	script_name = params["script_name"]
+  	@generated_script = ExportScript.export_script_by_sprint(sprint, script_name) 
+
+  	render "index",  generated_script: @generated_script 
   end
 
 end
