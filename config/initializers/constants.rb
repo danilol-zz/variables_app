31a8c6c6fbfa35beadf062e7620b26e6883c67a5
Danilo Lima <danilo.lima@victus.com.br>
2015-01-14 14:50:02 -0200

    Using constants

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -5,7 +5,6 @@ module Constants
   YES_NO = [ ["Sim", true], ["Não", false] ]
 
   #Origin
-  #Origem: tipo de base, periodicidade,tipo de retenção dos dados
   BASE_TYPE              = ["Arquivo Mainframe", "Tabela Mainframe", "Base Hadoop", "Outro"]
   PERIODICITY            = ["Diária", "Semanal", "Quinzenal", "Mensal", "Anual", "Exporádica", "Outro"]
   DATA_RETENTION_TYPE    = ["Movimento do dia", "Apenas registros alterados", "Histórica", "Acumulado de um período"]
@@ -14,7 +13,6 @@ module Constants
   MAINFRAME_STORAGE_TYPE = ["Disco", "Fita"]
 
   #OriginField
-  #Campo de Origem: tipo de dado, formato fmbase,  tipo de dadp generico, CD5 formato origem, CD5 Descrição formato origem
   DATA_TYPES             = ["Alfanumérico", "Numérico", "Compactado", "Data", "Numérico com vírgula", "Compactado com Vírgula", "Binário Mainframe"]
   FMBASE_FORMAT_TYPE     = ["AN", "ZD", "PD", "BI"]
   GENERIC_DATA_TYPE      = ["texto", "numero", "data"]
@@ -22,7 +20,6 @@ module Constants
   CD5_ORIGIN_FORMAT_DESC = ["character", "numeric", "Data"]
 
   #Variable
-  #Variavel: tipo de dado, Periodicidade da atualização SAS, Tipo de Dominio, Status Modelo de Dados SAS, Status DRS da Variavel
   DATA_TYPE              = ["Qtd", "Data", "Valor", "Indicador", "Nome", "Código", "Porcentagem", "Número"]
   SAS_UPDATE_PERIODICITY = ["diária", "semanal", "quinzenal", "mensal", "anual", "exporádica", "outro"]
   DOMAIN_TYPE            = ["interno", "externo", "fixo" ]
@@ -31,11 +28,9 @@ module Constants
   VARIABLE_TYPE          = ["calculado", "movimentação direta", "valor padrão"]
 
   #Campaign
-  #Campanha: Status de Levantamento de critério de fábrica, Tipo de processo
   FACTORY_CRITERION_STATUS = ["Ok", "Pendente"]
   PROCESS_TYPE             = ["Automático", "DBM", "Gestor", "Manual"]
 
   #Table
-  #Tabela: tipo de tabela
   TABLE_TYPE = ["seleção", "colagem", "lookup interna", "lookup externa", "lookup blindagem"]
 end
